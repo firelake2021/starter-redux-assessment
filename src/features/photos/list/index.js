37361b73b89ce40a8b7f76138de1f7bfcc5e3efb
Elena Krotkova <ekrotkova@indeed.com>
2023-11-29 15:10:15 -0600

    added new code

--- src/features/photos/list/index.js
+++ src/features/photos/list/index.js
@@ -1,20 +1,26 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import {
   // Task 7: Import the `removePhoto()` action creator from the photos slice
+  removePhoto,
   selectAllPhotos,
   // Task 13: Import the `selectFilteredPhotos()` selector from the photos slice
-} from '../photos.slice';
-import './list.css';
+  selectFilteredPhotos,
+} from "../photos.slice";
+import "./list.css";
 
 export default function PhotosList() {
   // Task 14: Call `useSelector()` below with `selectFilteredPhotos` instead of `selectAllPhotos`
-  const photos = useSelector(selectAllPhotos);
+  const photos = useSelector(selectFilteredPhotos);
   // Task 8: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
+  const dispatch = useDispatch();
 
   function handleDeleteButtonClick(id) {
     // Task 9: Dispatch the `removePhoto()` action creator, passing in the id
+    dispatch(removePhoto(id));
+    console.log("id to delete", id);
   }
 
+  console.log("photos", photos);
   const photosListItems = photos.map(({ id, caption, imageUrl }) => (
     <li key={id}>
       <img alt={caption} src={imageUrl} />
@@ -22,7 +28,8 @@ export default function PhotosList() {
         <p>{caption}</p>
         <button
           data-testid={`${caption}-button`}
-          onClick={() => handleDeleteButtonClick(id)}>
+          onClick={() => handleDeleteButtonClick(id)}
+        >
           Delete
         </button>
       </div>
--- src/features/suggestion/index.js
+++ src/features/suggestion/index.js
@@ -1,16 +1,18 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
   fetchSuggestion,
   selectError,
   selectLoading,
+  selectSuggestion,
   // Task 18: Import the `selectSuggestion()` selector from the suggestion slice
-} from './suggestion.slice';
-import './suggestion.css';
+} from "./suggestion.slice";
+import "./suggestion.css";
 
 export default function Suggestion() {
   // Task 19: Call useSelector() with the selectSuggestion() selector
   // The component needs to access the `imageUrl` and `caption` properties of the suggestion object.
+  const { imageUrl, caption } = useSelector(selectSuggestion);
   const loading = useSelector(selectLoading);
   const error = useSelector(selectError);
   const dispatch = useDispatch();
@@ -18,6 +20,7 @@ export default function Suggestion() {
   useEffect(() => {
     async function loadSuggestion() {
       // Task 20: Dispatch the fetchSuggestion() action creator
+      await dispatch(fetchSuggestion());
     }
     loadSuggestion();
   }, [dispatch]);
@@ -33,6 +36,8 @@ export default function Suggestion() {
       <>
         {/* <img alt={caption} src={imageUrl} />
         <p>{imageUrl}</p> */}
+        <img alt={caption} src={imageUrl} />
+        <p>{imageUrl}</p>
       </>
     );
   }
